69c90f69150e41f31363983608f7adcdc2f3721a
Shikhar Sharma <ceoshikhar@gmail.com>
2025-04-11 03:02:10 +0530

    wip: sidebar with toggle action. needs animation

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,32 +1,72 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { IconType } from "react-icons";
 
 import { MdGridView as IconDashboard } from "react-icons/md";
 import { TbChartCandle as IconTrades } from "react-icons/tb";
 import { LuNotebookPen as IconJournal } from "react-icons/lu";
+import { FaCircleChevronRight as IconRight } from "react-icons/fa6";
+import { FaCircleChevronLeft as IconLeft } from "react-icons/fa6";
 
 import { Logo } from "@/components/logo";
 import { Tooltip } from "@/components/tooltip";
 import { cn } from "@/lib/utils";
 import { APP_ROUTES } from "@/app-routes";
 
-interface SidebarProps {
-    open?: boolean;
-}
-
-export const Sidebar: FC<SidebarProps> = (props) => {
-    const { open = false } = props;
+export const Sidebar = () => {
+    const [open, setOpen] = useState(false);
 
     const { pathname } = useLocation();
     const navigate = useNavigate();
 
     return (
-        <div className="flex h-dvh flex-col px-3">
+        <div className="relative flex h-dvh flex-col px-3">
             <div>
-                <Logo className="m-auto mt-6" />
+                <div
+                    className={cn(
+                        "mt-6 flex w-full items-center justify-center",
+                        {
+                            "gap-3": open,
+                        }
+                    )}
+                >
+                    <Logo height={32} />
+                    <p
+                        className={cn("hidden", {
+                            "font-poppins text-primary-500 inline-block items-center text-2xl font-bold":
+                                open,
+                        })}
+                    >
+                        Arthveda
+                    </p>
+                </div>
 
-                <div className="mt-15 flex flex-col items-center gap-y-2">
+                <div className="absolute top-17 right-[-9px] opacity-50 transition-opacity hover:opacity-100">
+                    <button
+                        className="text-primary-400 cursor-pointer"
+                        onClick={() => setOpen((prev) => !prev)}
+                    >
+                        {open ? (
+                            <Tooltip
+                                content="Collapse"
+                                delayDuration={300}
+                                contentProps={{ side: "left" }}
+                            >
+                                <IconLeft size={20} />
+                            </Tooltip>
+                        ) : (
+                            <Tooltip
+                                content="Expand"
+                                delayDuration={300}
+                                contentProps={{ side: "right" }}
+                            >
+                                <IconRight size={20} />
+                            </Tooltip>
+                        )}
+                    </button>
+                </div>
+
+                <div className="mt-12 flex flex-col items-center gap-y-2">
                     <SidebarNavItem
                         label="Dashboard"
                         Icon={IconDashboard}
@@ -65,19 +105,24 @@ interface SidebarNavItemProps {
 const SidebarNavItem: FC<SidebarNavItemProps> = (props) => {
     const { label, Icon, open, isActive, onClick } = props;
 
-    const icon = (
+    const content = (
         <div
-            className={cn(
-                "text-primary-600 hover:bg-primary-950 active:bg-primary-900 active:text-primary-400 rounded-lg p-3 transition hover:cursor-pointer",
-                {
-                    "bg-primary-900 text-primary-50": isActive,
-                }
-            )}
+            className={cn("text-primary-600 w-fit rounded-lg p-3", {
+                "bg-primary-900 text-primary-100": isActive,
+                "hover:bg-primary-950 active:bg-primary-900 active:text-primary-400 hover:cursor-pointer":
+                    !isActive,
+                "flex w-[240px] gap-3": open,
+            })}
             onClick={onClick}
         >
             <Icon size={24} />
+            {open && <p>{label}</p>}
         </div>
     );
+
+    if (open) {
+        return content;
+    }
 
     return (
         <Tooltip
@@ -85,7 +130,7 @@ const SidebarNavItem: FC<SidebarNavItemProps> = (props) => {
             delayDuration={0}
             contentProps={{ side: "right" }}
         >
-            {icon}
+            {content}
         </Tooltip>
     );
 };
--- frontend/src/protected-route-layout.tsx
+++ frontend/src/protected-route-layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./components/sidebar";
 
 export const ProtectedRouteLayout: FC<PropsWithChildren> = ({ children }) => {
     return (
-        <div className="flex">
+        <div className="flex gap-x-4">
             <Sidebar />
             <div>{children}</div>
         </div>
