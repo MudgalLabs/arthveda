196f1e2438374ae126b1cdd64c235f8ca850eb32
Shikhar Sharma <ceoshikhar@gmail.com>
2025-04-21 15:57:50 +0530

    chore: clean up dashboard

--- frontend/src/features/dashboard/dashboard.tsx
+++ frontend/src/features/dashboard/dashboard.tsx
@@ -1,18 +1,7 @@
-import { toast } from "@/components/toast";
-import { Button } from "@/s8ly";
-
 export const Dashboard = () => {
-    const handleClick = () => {
-        toast("Hello");
-        toast.info("Info");
-        toast.success("Success");
-        toast.error("Error");
-    };
-
     return (
         <>
             <h1>Dashboard</h1>
-            <Button onClick={handleClick}>Toast</Button>
         </>
     );
 };
