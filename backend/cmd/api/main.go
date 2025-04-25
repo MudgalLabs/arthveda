e9ed07f6ee8b84b4a203f21a0fdcba67045dc15c
Shikhar Sharma <ceoshikhar@gmail.com>
2025-04-25 16:10:26 +0530

    refactor: move initRouter to routes.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"arthveda/internal/apires"
 	"arthveda/internal/db"
 	"arthveda/internal/env"
 	"arthveda/internal/logger"
@@ -12,10 +11,6 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -38,55 +33,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func initRouter() http.Handler {
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(attachLoggerMiddleware)
-	r.Use(logRequestMiddleware)
-	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"*"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-		AllowOriginFunc: func(r *http.Request, origin string) bool {
-			return true
-		},
-	}))
-
-	// Set a timeout value on the request context (ctx), that will signal
-	// through ctx.Done() that the request has timed out and further
-	// processing should be stopped.
-	r.Use(middleware.Timeout(60 * time.Second))
-
-	r.Route("/api/v1", func(r chi.Router) {
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			writeJSON(w, http.StatusOK, apires.Success(http.StatusOK, "Hi! Welcome to Arthveda API. Don't be naughty.", nil))
-		})
-
-		r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-			writeJSON(w, http.StatusOK, apires.Success(http.StatusOK, "Pong", nil))
-		})
-
-		r.Route("/auth", func(r chi.Router) {
-			r.Post("/signup", signUpHandler)
-			r.Post("/signin", signInHandler)
-			r.Post("/signout", signOutHandler)
-		})
-
-		r.Route("/user", func(r chi.Router) {
-			r.Use(authMiddleware)
-			r.Get("/me", getMeHandler)
-		})
-	})
-
-	return r
 }
 
 func run(router http.Handler) error {
---
+++ backend/cmd/api/routes.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func initRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(attachLoggerMiddleware)
+	r.Use(logRequestMiddleware)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		AllowOriginFunc: func(r *http.Request, origin string) bool {
+			return true
+		},
+	}))
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			successResponse(w, r, http.StatusOK, "Hi! Welcome to Arthveda API. Don't be naughty.", nil)
+		})
+
+		r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+			successResponse(w, r, http.StatusOK, "Pong", nil)
+		})
+
+		r.Route("/auth", func(r chi.Router) {
+			r.Post("/signup", signUpHandler)
+			r.Post("/signin", signInHandler)
+			r.Post("/signout", signOutHandler)
+		})
+
+		r.Route("/user", func(r chi.Router) {
+			r.Use(authMiddleware)
+			r.Get("/me", getMeHandler)
+		})
+	})
+
+	return r
+}
