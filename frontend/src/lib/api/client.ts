13def58360105a22a2cb68850d0f229f595de032
Shikhar Sharma <ceoshikhar@gmail.com>
2025-04-30 01:33:24 +0530

    devops: test env variables on cloudflare pages

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
 import { isProd } from "@/lib/utils";
 
-const baseURL = isProd() ? "/api/v1" : "http://localhost:1337/api/v1";
+console.log({
+    "vite.VITE_CLOUDFLARE_PAGES": import.meta.env.VITE_CLOUDFLARE_PAGES,
+    "vite.VITE_API_V1_URL": import.meta.env.VITE_API_V1_URL,
+    "import.meta.env": import.meta.env,
+});
+
+let baseURL = isProd() ? "/api/v1" : "http://localhost:1337/api/v1";
+
+console.log("before", { baseURL });
+
+if (import.meta.env.VITE_CLOUDFLARE_PAGES === "yes") {
+    baseURL = import.meta.env.VITE_API_V1_URL;
+}
+
+console.log("after", { baseURL });
 
 export const client = axios.create({
     baseURL,
