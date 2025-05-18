817ca5fb0bb48d3a5d9ef8229046411e10c3d09e
Shikhar Sharma <ceoshikhar@gmail.com>
2025-05-18 20:07:01 +0530

    fix: change int to *int for nullable column

--- server/internal/features/user_identity/domain.go
+++ server/internal/features/user_identity/domain.go
@@ -14,7 +14,7 @@ type UserIdentity struct {
 	Email               string     `json:"email" db:"email"`
 	PasswordHash        string     `json:"password_hash" db:"password_hash"`
 	Verified            bool       `json:"verified" db:"verified"`
-	FailedLoginAttempts int        `json:"failed_login_attempts" db:"failed_login_attempts"`
+	FailedLoginAttempts *int       `json:"failed_login_attempts" db:"failed_login_attempts"`
 	LastLoginAt         *time.Time `json:"last_login_at" db:"last_login_at"`
 	CreatedAt           time.Time  `json:"created_at" db:"created_at"`
 	UpdatedAt           *time.Time `json:"updated_at" db:"updated_at"`
@@ -33,12 +33,14 @@ func newUserIdentity(email, password string) (*UserIdentity, error) {
 		return nil, fmt.Errorf("uuid: %w", err)
 	}
 
+	zeroAttempts := 0
+
 	userIdentity := &UserIdentity{
 		ID:                  ID,
 		Email:               email,
 		PasswordHash:        string(passwordHashBytes),
 		Verified:            false,
-		FailedLoginAttempts: 0,
+		FailedLoginAttempts: &zeroAttempts,
 		CreatedAt:           time.Now().UTC(),
 	}
 
