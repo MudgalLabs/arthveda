d6f1b805434bda702b0df561c09fc5cf484cc4bb
Shikhar Sharma <ceoshikhar@gmail.com>
2025-06-05 01:04:53 +0530

    ci: fix env vars mismatch

--- server/internal/env/env.go
+++ server/internal/env/env.go
@@ -20,7 +20,7 @@ var (
 )
 
 func IsProd() bool {
-	return os.Getenv("APP_ENV") == "production"
+	return os.Getenv("GO_APP_ENV") == "production"
 }
 
 // NOTE: We only read variables in prod, in dev we have hard coded everything.
@@ -30,24 +30,24 @@ func Init() {
 	if IsProd() {
 		APP_ENV = "production"
 		PORT = os.Getenv("GO_PORT")
+		JWT_SECRET = os.Getenv("GO_JWT_SECRET")
+		LOG_LEVEL = os.Getenv("GO_LOG_LEVEL")
+		LOG_FILE = os.Getenv("GO_LOG_FILE")
 		DB_HOST = os.Getenv("DB_HOST")
 		DB_PORT = os.Getenv("DB_PORT")
 		DB_NAME = os.Getenv("DB_NAME")
 		DB_USER = os.Getenv("DB_USER")
 		DB_PASSWORD = os.Getenv("DB_PASSWORD")
-		JWT_SECRET = os.Getenv("GO_JWT_SECRET")
-		LOG_LEVEL = os.Getenv("GO_LOG_LEVEL")
-		LOG_FILE = os.Getenv("GO_LOG_FILE")
 	} else {
 		APP_ENV = "development"
 		PORT = "1337"
+		JWT_SECRET = "this_is_a_very_strong_jwt_secret_i_promise"
+		LOG_LEVEL = "error"
+		LOG_FILE = "arthveda_go.log"
 		DB_HOST = "localhost"
 		DB_PORT = "42069"
 		DB_NAME = "postgres"
 		DB_USER = "postgres"
 		DB_PASSWORD = "postgres"
-		JWT_SECRET = "this_is_a_very_strong_jwt_secret_i_promise"
-		LOG_LEVEL = "error"
-		LOG_FILE = "arthveda_go.log"
 	}
 }
