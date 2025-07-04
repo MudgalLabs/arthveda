6a2d9f3ad55a463f581d40b032561d6aa4d9c299
Shikhar Sharma <ceoshikhar@gmail.com>
2025-07-04 23:40:35 +0530

    chore: refactor the realisedpositions filtering logic, pass pnlbuckets to cum pnl buckets function

--- api/internal/feature/dashboard/core.go
+++ api/internal/feature/dashboard/core.go
@@ -36,100 +36,12 @@ func getGeneralStats(positions []*position.Position, end time.Time, loc *time.Lo
 		return generalStats{}
 	}
 
-	originalPosByID := map[uuid.UUID]*position.Position{}
-
-	// These are the trades that we will use to compute the stats.
-	// We will only consider trades that are before or equal to the end date.
-	// We will also create a copy of the positions so that we don't modify the original positions
-	// and their trades. This is important because we will be calling "Compute" on the positions
-	// to calculate the realised PnL and other stats, and we don't want to modify the original positions.
-	positionsWithTradesUptoEnd := []position.Position{}
-
-	// We need to compute the Position stats based on the trades that fall within the date range.
-	// So we will go through all positions and their trades,
-	// and call "Compute" up until we don't reach a trade that's time is after the end date.
-	// If we reach a trade that is after the end date, we will stop processing the position.
-
-	for _, p := range positions {
-		originalPosByID[p.ID] = p
-		positionCopy := *p
-		trades := []*trade.Trade{}
-
-		atLeastOneTradeWasRealisingPnL := false
-
-		// Apply trades to the position to calculate realised PnL.
-		// This will also update the position's GrossPnLAmount, NetPnLAmount
-		// and TotalChargesAmount fields.
-		for _, t := range p.Trades {
-			if t.Time.In(loc).Before(end) || t.Time.In(loc).Equal(end) {
-				trades = append(trades, t)
-
-				if position.IsTradeRealisingPnL(t, p) {
-					atLeastOneTradeWasRealisingPnL = true
-				}
-			}
-		}
-
-		positionCopy.Trades = trades
-
-		if atLeastOneTradeWasRealisingPnL {
-			positionsWithTradesUptoEnd = append(positionsWithTradesUptoEnd, positionCopy)
-		}
-	}
-
-	// Let's call "Compute" on positionsWithTradesUptoEnd
-	// to calculate the realised PnL and other stats.
-
-	for i, p := range positionsWithTradesUptoEnd {
-		payload := position.ComputePayload{
-			Trades:     position.ConvertTradesToCreatePayload(p.Trades),
-			RiskAmount: p.RiskAmount,
-		}
-
-		computeResult, err := position.Compute(payload)
-		if err != nil {
-			// If we fail silently and continue.
-			logger.Get().Errorw("Failed to compute position", "error", err, "symbol", p.Symbol, "opened_at", p.OpenedAt)
-			continue
-		}
-
-		position.ApplyComputeResultToPosition(&p, computeResult)
-		positionsWithTradesUptoEnd[i] = p
-	}
-
-	// chargesDiffTotal := decimal.Zero
-	// chargesDiffCount := 0
-
-	// for _, p := range positionsWithTradesUptoEnd {
-	// 	origPos, exists := originalPosByID[p.ID]
-	// 	if !exists {
-	// 		panic("gg")
-	// 	}
-
-	// 	if !origPos.GrossPnLAmount.Equal(p.GrossPnLAmount) {
-	// 		fmt.Printf("Position %s has different GrossPnLAmount: original=%s, computed=%s\n",
-	// 			origPos.ID, origPos.GrossPnLAmount.String(), p.GrossPnLAmount.String())
-	// 	}
-
-	// 	if !origPos.TotalChargesAmount.Equal(p.TotalChargesAmount) {
-	// 		fmt.Printf("Position %s has different TotalChargesAmount: original=%s, computed=%s\n",
-	// 			origPos.ID, origPos.TotalChargesAmount.String(), p.TotalChargesAmount.String())
-	// 		diff := origPos.TotalChargesAmount.Sub(p.TotalChargesAmount).Abs()
-	// 		chargesDiffTotal = chargesDiffTotal.Add(diff)
-	// 		chargesDiffCount++
-	// 	}
-	// }
-
-	// fmt.Printf("Total original positions: %d\n", len(originalPosByID))
-	// fmt.Printf("Total positions with trades up to end: %d\n", len(positionsWithTradesUptoEnd))
-	// fmt.Printf("Total charges difference across %d positions: %s\n", chargesDiffCount, chargesDiffTotal.String())
-
 	var winRate float64
 	var grossPnL, netPnL, charges, avgRFactor, avgWinRFactor, avgLossRFactor, avgWin, avgLoss, maxWin, maxLoss decimal.Decimal
 	var openTradesCount, settledTradesCount, winTradesCount, lossTradesCount int
 	var maxWinStreak, maxLossStreak, currentWin, currentLoss int
 
-	for _, p := range positionsWithTradesUptoEnd {
+	for _, p := range positions {
 		// Calculate open trades count.
 		// Will be used to calculate win rate.
 		if p.Status == position.StatusOpen {
@@ -191,7 +103,7 @@ func getGeneralStats(positions []*position.Position, end time.Time, loc *time.Lo
 	}
 
 	// Trades that are not open are considered settled.
-	settledTradesCount = len(positionsWithTradesUptoEnd) - openTradesCount
+	settledTradesCount = len(positions) - openTradesCount
 	// Trades that are settled and not winning are considered losing.
 	lossTradesCount = settledTradesCount - winTradesCount
 
@@ -249,31 +161,22 @@ type pnlBucket struct {
 	TradesBySymbol pnlBucketTradesBySymbol `json:"trades_by_symbol"`
 }
 
-// getCumulativePnLBuckets calculates cumulative realized PnL across time buckets
-// (daily, weekly, or monthly) for the given positions and their associated trades.
-//
-// Notes:
-// - `positions` includes all position that have trade(s) that fall within [start, end].
-// - Trades are assumed to be in UTC.
-// - Charges are considered in this calculation to compute NetPnL.
-// - Uses `position.ApplyTradeToPosition` to calculate realized PnL from partial or full exits.
-func getCumulativePnLBuckets(positions []*position.Position, period common.BucketPeriod, start, end time.Time, loc *time.Location) []pnlBucket {
-	results := getPnLBuckets(positions, period, start, end, loc)
-
+// getCumulativePnLBuckets calculates cumulative realized PnL using pnL buckets.
+func getCumulativePnLBuckets(pnlBuckets []pnlBucket) []pnlBucket {
 	// Convert bucket PnL and charges to cumulative values with rounding
-	for i := range results {
+	for i := range pnlBuckets {
 		if i > 0 {
-			results[i].NetPnL = results[i].NetPnL.Add(results[i-1].NetPnL)
-			results[i].GrossPnL = results[i].GrossPnL.Add(results[i-1].GrossPnL)
-			results[i].Charges = results[i].Charges.Add(results[i-1].Charges)
+			pnlBuckets[i].NetPnL = pnlBuckets[i].NetPnL.Add(pnlBuckets[i-1].NetPnL)
+			pnlBuckets[i].GrossPnL = pnlBuckets[i].GrossPnL.Add(pnlBuckets[i-1].GrossPnL)
+			pnlBuckets[i].Charges = pnlBuckets[i].Charges.Add(pnlBuckets[i-1].Charges)
 		}
 		// Round final cumulative values to match database precision
-		results[i].NetPnL = results[i].NetPnL.Round(2)
-		results[i].GrossPnL = results[i].GrossPnL.Round(2)
-		results[i].Charges = results[i].Charges.Round(2)
+		pnlBuckets[i].NetPnL = pnlBuckets[i].NetPnL.Round(2)
+		pnlBuckets[i].GrossPnL = pnlBuckets[i].GrossPnL.Round(2)
+		pnlBuckets[i].Charges = pnlBuckets[i].Charges.Round(2)
 	}
 
-	return results
+	return pnlBuckets
 }
 
 func getPnLBuckets(positions []*position.Position, period common.BucketPeriod, start, end time.Time, loc *time.Location) []pnlBucket {
@@ -357,4 +260,106 @@ func getPnLBuckets(positions []*position.Position, period common.BucketPeriod, s
 	}
 
 	return results
+}
+
+func filterPositionsWithRealisingTradesUpTo(positions []*position.Position, end time.Time, loc *time.Location) []*position.Position {
+	// These are the trades that we will use to compute the stats.
+	// We will only consider trades that are before or equal to the end date.
+	// We will also create a copy of the positions so that we don't modify the original positions
+	// and their trades. This is important because we will be calling "Compute" on the positions
+	// to calculate the realised PnL and other stats, and we don't want to modify the original positions.
+
+	// We need to compute the Position stats based on the trades that fall within the date range.
+	// So we will go through all positions and their trades,
+	// and call "Compute" up until we don't reach a trade that's time is after the end date.
+	// If we reach a trade that is after the end date, we will stop processing the position.
+	positionsWithTradesUptoEnd := []*position.Position{}
+
+	if len(positions) == 0 || positions == nil {
+		return positionsWithTradesUptoEnd
+	}
+
+	originalPosByID := map[uuid.UUID]*position.Position{}
+	for _, p := range positions {
+		originalPosByID[p.ID] = p
+		positionCopy := *p
+		trades := []*trade.Trade{}
+
+		atLeastOneTradeWasScalingOut := false
+
+		// Apply trades to the position to calculate realised PnL.
+		// This will also update the position's GrossPnLAmount, NetPnLAmount
+		// and TotalChargesAmount fields.
+		for _, t := range p.Trades {
+			if t.Time.In(loc).Before(end) || t.Time.In(loc).Equal(end) {
+				trades = append(trades, t)
+
+				// If position is long and we have a sell trade,
+				// or if position is short and we have a buy trade,
+				// we know that this is a scaling out trade.
+				// This flag helps us to include positions for calculating stats
+				// that have tried to realise PnL by scaling out. Otherwise, we might have
+				// wrong stats for positions that were just scaling in during the time range.
+				if (positionCopy.Direction == position.DirectionLong && t.Kind == trade.TradeKindSell) ||
+					(positionCopy.Direction == position.DirectionShort && t.Kind == trade.TradeKindBuy) {
+					atLeastOneTradeWasScalingOut = true
+				}
+			}
+		}
+
+		positionCopy.Trades = trades
+
+		if atLeastOneTradeWasScalingOut {
+			positionsWithTradesUptoEnd = append(positionsWithTradesUptoEnd, &positionCopy)
+		}
+	}
+
+	// Let's call "Compute" on positionsWithTradesUptoEnd
+	// to calculate the realised PnL and other stats.
+
+	for i, p := range positionsWithTradesUptoEnd {
+		payload := position.ComputePayload{
+			Trades:     position.ConvertTradesToCreatePayload(p.Trades),
+			RiskAmount: p.RiskAmount,
+		}
+
+		computeResult, err := position.Compute(payload)
+		if err != nil {
+			// If we fail silently and continue.
+			logger.Get().Errorw("Failed to compute position", "error", err, "symbol", p.Symbol, "opened_at", p.OpenedAt)
+			continue
+		}
+
+		position.ApplyComputeResultToPosition(p, computeResult)
+		positionsWithTradesUptoEnd[i] = p
+	}
+
+	// chargesDiffTotal := decimal.Zero
+	// chargesDiffCount := 0
+
+	// for _, p := range positionsWithTradesUptoEnd {
+	// 	origPos, exists := originalPosByID[p.ID]
+	// 	if !exists {
+	// 		panic("gg")
+	// 	}
+
+	// 	if !origPos.GrossPnLAmount.Equal(p.GrossPnLAmount) {
+	// 		fmt.Printf("Position %s has different GrossPnLAmount: original=%s, computed=%s\n",
+	// 			origPos.ID, origPos.GrossPnLAmount.String(), p.GrossPnLAmount.String())
+	// 	}
+
+	// 	if !origPos.TotalChargesAmount.Equal(p.TotalChargesAmount) {
+	// 		fmt.Printf("Position %s has different TotalChargesAmount: original=%s, computed=%s\n",
+	// 			origPos.ID, origPos.TotalChargesAmount.String(), p.TotalChargesAmount.String())
+	// 		diff := origPos.TotalChargesAmount.Sub(p.TotalChargesAmount).Abs()
+	// 		chargesDiffTotal = chargesDiffTotal.Add(diff)
+	// 		chargesDiffCount++
+	// 	}
+	// }
+
+	// fmt.Printf("Total original positions: %d\n", len(originalPosByID))
+	// fmt.Printf("Total positions with trades up to end: %d\n", len(positionsWithTradesUptoEnd))
+	// fmt.Printf("Total charges difference across %d positions: %s\n", chargesDiffCount, chargesDiffTotal.String())
+
+	return positionsWithTradesUptoEnd
 }
--- api/internal/feature/dashboard/service.go
+++ api/internal/feature/dashboard/service.go
@@ -4,7 +4,6 @@ import (
 	"arthveda/internal/common"
 	"arthveda/internal/feature/position"
 	"arthveda/internal/feature/trade"
-	"arthveda/internal/logger"
 	"arthveda/internal/service"
 	"context"
 	"time"
@@ -112,82 +111,20 @@ func (s *Service) Get(ctx context.Context, userID uuid.UUID, loc *time.Location,
 		bucketPeriod = common.BucketPeriodMonthly
 	}
 
-	originalPosByID := map[uuid.UUID]*position.Position{}
+	positionsFiltered := filterPositionsWithRealisingTradesUpTo(positions, end, loc)
 
-	// These are the trades that we will use to compute the stats.
-	// We will only consider trades that are before or equal to the end date.
-	// We will also create a copy of the positions so that we don't modify the original positions
-	// and their trades. This is important because we will be calling "Compute" on the positions
-	// to calculate the realised PnL and other stats, and we don't want to modify the original positions.
-	positionsWithTradesUptoEnd := []*position.Position{}
+	generalStats := getGeneralStats(positionsFiltered, end, loc)
+	pnlBuckets := getPnLBuckets(positionsFiltered, bucketPeriod, start, end, loc)
 
-	// We need to compute the Position stats based on the trades that fall within the date range.
-	// So we will go through all positions and their trades,
-	// and call "Compute" up until we don't reach a trade that's time is after the end date.
-	// If we reach a trade that is after the end date, we will stop processing the position.
-
-	for _, p := range positions {
-		originalPosByID[p.ID] = p
-		positionCopy := *p
-		trades := []*trade.Trade{}
-
-		atLeastOneTradeWasScalingOut := false
-
-		// Apply trades to the position to calculate realised PnL.
-		// This will also update the position's GrossPnLAmount, NetPnLAmount
-		// and TotalChargesAmount fields.
-		for _, t := range p.Trades {
-			if t.Time.In(loc).Before(end) || t.Time.In(loc).Equal(end) {
-				trades = append(trades, t)
-
-				// If position is long and we have a sell trade,
-				// or if position is short and we have a buy trade,
-				// we know that this is a scaling out trade.
-				// This flag helps us to include positions for calculating stats
-				// that have tried to realise PnL by scaling out. Otherwise, we might have
-				// wrong stats for positions that were just scaling in during the time range.
-				if (positionCopy.Direction == position.DirectionLong && t.Kind == trade.TradeKindSell) ||
-					(positionCopy.Direction == position.DirectionShort && t.Kind == trade.TradeKindBuy) {
-					atLeastOneTradeWasScalingOut = true
-				}
-			}
-		}
-
-		positionCopy.Trades = trades
-
-		if atLeastOneTradeWasScalingOut {
-			positionsWithTradesUptoEnd = append(positionsWithTradesUptoEnd, &positionCopy)
-		}
-	}
-
-	// Let's call "Compute" on positionsWithTradesUptoEnd
-	// to calculate the realised PnL and other stats.
-
-	for i, p := range positionsWithTradesUptoEnd {
-		payload := position.ComputePayload{
-			Trades:     position.ConvertTradesToCreatePayload(p.Trades),
-			RiskAmount: p.RiskAmount,
-		}
-
-		computeResult, err := position.Compute(payload)
-		if err != nil {
-			// If we fail silently and continue.
-			logger.Get().Errorw("Failed to compute position", "error", err, "symbol", p.Symbol, "opened_at", p.OpenedAt)
-			continue
-		}
-
-		position.ApplyComputeResultToPosition(p, computeResult)
-		positionsWithTradesUptoEnd[i] = p
-	}
-
-	generalStats := getGeneralStats(positions, end, loc)
-	pnlBuckets := getPnLBuckets(positionsWithTradesUptoEnd, bucketPeriod, start, end, loc)
-	cumulativePnLBuckets := getCumulativePnLBuckets(positionsWithTradesUptoEnd, bucketPeriod, start, end, loc)
+	// Make a copy of the buckets to avoid modifying the original slice
+	pnlBucketsCopy := make([]pnlBucket, len(pnlBuckets))
+	copy(pnlBucketsCopy, pnlBuckets)
+	cumulativePnLBuckets := getCumulativePnLBuckets(pnlBucketsCopy)
 
 	result := &GetDashboardReponse{
+		Positions:            positionsFiltered,
+		PositionsCount:       len(positionsFiltered),
 		generalStats:         generalStats,
-		Positions:            positions,
-		PositionsCount:       len(positions),
 		CumulativePnLBuckets: cumulativePnLBuckets,
 		PnLBuckets:           pnlBuckets,
 	}
